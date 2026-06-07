cea940088ad01748c54fe1f5fe2388a05ec41029
Dwisna <158534538+Dwisna@users.noreply.github.com>
2026-06-08 05:34:17 +0700

    update modelling use preprocessed dataset

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -4,31 +4,29 @@ import mlflow
 import mlflow.sklearn
 import os
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
-# === Load Dataset ===
+# === Load Dataset yang sudah dipreprocessing ===
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-df = pd.read_csv(os.path.join(BASE_DIR, 'Social_Network_Ads.csv'))
+df = pd.read_csv(os.path.join(BASE_DIR, 'Social_Network_Ads_preprocessing.csv'))
 print("✅ Dataset berhasil dimuat!")
 print("Shape:", df.shape)
 
-# === Preprocessing ===
-df = df.drop(columns=['User ID'])
-le = LabelEncoder()
-df['Gender'] = le.fit_transform(df['Gender'])
-
+# === Pisahkan fitur dan target ===
 X = df[['Gender', 'Age', 'EstimatedSalary']]
 y = df['Purchased']
 
+# === Split data ===
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
+# === Normalisasi ===
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled  = scaler.transform(X_test)
+X_test_scaled = scaler.transform(X_test)
 
 # === MLflow Tracking ===
 mlflow.sklearn.autolog()
